93c6b82b6ca424169de37cd97c8095cc9d0e403c
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 17:03:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,12 +5,8 @@ def hello_t(array)
     yield array[i]
     i = i + 1
   end
+  array
 end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
